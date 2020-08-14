aa929db56d4bd0bea56b81106f728129137cf36d
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2020-08-14 13:03:22 +0500

    Created ItemModel

---
+++ BasicGeneralStoreDesktopApplication/Model/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicGeneralStoreDesktopApplication.Model
+{
+    public class Item
+    {
+        public String Name { get; set; }
+        public decimal BuyingPrice { get; set; }
+        public decimal SellingPrice { get; set; }
+        public decimal Quantity { get; set; }
+        public String Unit { get; set; }
+        public DateTime UpdatedOn { get; set; }
+
+    }
+}
