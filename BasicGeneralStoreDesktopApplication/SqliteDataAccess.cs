ba2ed715770a33ca7440a2e29c0b07591439642f
Zakawat <31027252+zsubzwary@users.noreply.github.com>
2020-08-14 16:45:53 +0500

    Add code to save ITEM to database.

--- BasicGeneralStoreDesktopApplication/SqliteDataAccess.cs
+++ BasicGeneralStoreDesktopApplication/SqliteDataAccess.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BasicGeneralStoreDesktopApplication.Model;
+using Dapper;
 
 namespace BasicGeneralStoreDesktopApplication
 {
@@ -18,6 +21,14 @@ namespace BasicGeneralStoreDesktopApplication
         public static List<Item> getAllItems()
         {
             return null;
+        }
+
+        public static void SaveItem(Item item)
+        {
+            using (IDbConnection dbConnection = new SQLiteConnection(getConnectionString()))
+            {
+                dbConnection.Execute("INSERT INTO Item (Name, BuyingPrice, SellingPrice, Quantity, Unit) VALUES( @Name, @BuyingPrice, @SellingPrice, @Quantity, @Unit ); ", item);
+            }
         }
 
     }
